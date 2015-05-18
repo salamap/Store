66a2d4953a54fa0dbf72b9efb5d87d1505e10e8c
Peter Salama <salamap@gmail.com>
2015-05-17 17:49:49 -0700

    removing console logs

--- server/bizLogic.js
+++ server/bizLogic.js
@@ -101,8 +101,6 @@ if (Meteor.isServer) {
     },
 
     doExchange: function(arrayA, arrayB) {
-      console.log(arrayA);
-      console.log(arrayB);
       var user = Meteor.user();
       if (!user) {
         throw new Meteor.Error(401, 'You need to login!');
